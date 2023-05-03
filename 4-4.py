1ceae1bdd498e4e86d01eb5fe7178f8f619f9f79
Yao <pumpkinjiang@126.com>
2023-05-03 17:25:03 +0100

    Changes to be committed: modified:   4-4.py

--- 4-4.py
+++ 4-4.py
@@ -1,30 +1,26 @@
 
 list=['CATATGGG','CATCTGGT','CATATGAT','CTTCCGGC','CATGAGGC','CATCTCGC','CAAATGGC','CATATGGC']
-
+print(len(list))
 
 def Consensus(list):
     'finds the consensus sequence between a set of sequences with the same length'
     output=''
 
     #TEST  length
-    #for i in range(0,len(list)-1):
-     #   if len(list[i]) != len(list[i+1]):
-      #      print('Sequences should be in the same lenth.')
-    running= True
-    while running:
-        i=0
-        if i < len(list):
-                if  len(list[i]) == len(list[i+1]):
-                    i= 1+1
-                print(i)
-           
-        else:
+    for i in range(0,len(list)-1):
+        if len(list[i]) != len(list[i+1]):
             print('Sequences should be in the same lenth.')
-            running = False
-        if i == len(list):
-                print('OK')
-                running = False
-    return i
+        elif i == len(list)-2:
+            print('OK')
+
+    ## Compare sequence
+    #print(len(list[0]))
+    for j in range (0,len(list)-1):
+        for k in range (0,len(list[i])):
+            
+
+    
+        
         
 
 
